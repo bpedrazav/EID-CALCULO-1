592fa2ced05f6d0045fbf9aa8f2c6da3c2b8103d
bpedrazav <bpedraza2026@alu.uct.cl>
2026-06-09 22:19:17 -0400

    creacion de algunas funciones del programa

--- Calculo 1 - EID.py
+++ Calculo 1 - EID.py
@@ -10,3 +10,19 @@ ctk.set_default_color_theme("blue")
 programa_calculo = ctk.CTk()
 programa_calculo.title("EID-CALCULO-LIMITES")
 programa_calculo.geometry("1080x720")
+
+
+#_____________________________________creación de las funciones del programa______________________________________________
+#función limpiar texto
+def limpiar():
+    ingresar_datos.delete("1.0", "end")
+    ingresar_tendencia.delete("1.0", "end")
+    resultado.configure(text="Resultado del límite:")
+
+#funcion coseno
+def coseno():
+    ingresar_datos.insert("end", "cos(x)") 
+
+#funcion seno
+def sin():
+    ingresar_datos.insert("end","sin(x)")
